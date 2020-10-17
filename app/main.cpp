d88f5bcc15c35b0f1d02b576b7cedf5c068d8d17
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-10-17 16:30:42 -0500

    Kruskal with priority queue

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 #define cimg_use_jpeg
 #include "CImg.h"
@@ -30,22 +31,68 @@ double distance(vector<double> v1, vector<double> v2) {
 	return total;
 }
 
-int main() {
-	CImg<double> A("data/example1.jpg");
-	CImg<double> B("data/example2.jpg");
-	CImg<double> C("data/example3.jpg");
-	
-	A.resize(384, 288);
-	B.resize(384, 288);
-	C.resize(384, 288);
-	
-	vector<double> vA = vectorize(A);
-	vector<double> vB = vectorize(B);
-	vector<double> vC = vectorize(C);
+vector<vector<double> > get_vectors() {
+	vector<vector<double> > points;
 
-	cout << "A - B distance: " << distance(vA, vB) << endl;
-	cout << "A - C distance: " << distance(vA, vC) << endl;
-	cout << "B - C distance: " << distance(vB, vC) << endl;
+	CImg<double> A("data/example1.jpg");
+	A.resize(384, 288);
+	vector<double> vA = vectorize(A);
+	points.push_back(vA);
+	
+	CImg<double> B("data/example2.jpg");
+	B.resize(384, 288);
+	vector<double> vB = vectorize(B);
+	points.push_back(vB);
+	
+	CImg<double> C("data/example3.jpg");
+	C.resize(384, 288);
+	vector<double> vC = vectorize(C);
+	points.push_back(vC);
+
+	return points;
+}
+
+struct GraphEdge {
+	vector<double> v1;
+	vector<double> v2;
+	double distance;
+};
+
+struct CompareGraphEdge {
+	bool operator()(GraphEdge &e1, GraphEdge &e2) {
+		return e1.distance < e2.distance;
+	}
+};
+
+int main() {
+	vector<vector<double> > points = get_vectors();
+
+	int n = points.size();
+	int k = 2; // Number of clusters
+
+	priority_queue<GraphEdge, vector<GraphEdge>, CompareGraphEdge> edges;
+
+	for (int i = 0; i < points.size(); i++) {
+		for (int j = i + 1; j < points.size(); j++) {
+			GraphEdge g;
+
+			g.v1 = points[i];
+			g.v2 = points[j];
+			g.distance = distance(g.v1, g.v2);
+
+			edges.push(g);
+		}
+	}
+
+	while (edges.size() > n - k) edges.pop();
+
+	while (edges.size() > 0) {
+		auto e = edges.top();
+
+		cout << e.distance << endl;
+
+		edges.pop();
+	}
 	
 	return 0;
 }
